76c739ecf09b9b377b8d77be40f809bec7435cc5
andreyborovkov <andrey_borovkov@yahoo.com>
2019-02-15 16:13:33 -0500

    js for picture animations

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+
 /**Fixed navigation bar */
-    const nav = document.querySelector('#main');
-    const topOfNav = nav.offsetTop;
-  
-    function fixNav() {
-      if(window.scrollY >= topOfNav){
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed-nav');
-      } else {
-        document.body.style.paddingTop = 0;
-        document.body.classList.remove('fixed-nav');
-      }
-    }
-  
-    window.addEventListener('scroll', fixNav);
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if(window.scrollY >= topOfNav){
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+function changeIndex() {
+  document.querySelector(".rotate_right").style.zIndex = "2";
+}
+
+window.addEventListener('scroll', fixNav);
